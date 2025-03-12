b57d091cf814d4f68cfc305edd7545f13a5ef859
wu <wu@.com>
2025-03-12 13:07:02 +0800

    mathandy/svgpathtools0

--- CHANGELOG.rst
+++
@@ -1,4 +0,0 @@
-CHANGELOG
-============
-
-See `Releases <https://github.com/pytest-dev/nose2pytest/releases>`__ for the changelog of each version.
--- setup.py
+++ setup.py
@@ -1,42 +1,59 @@
 from setuptools import setup
+import codecs
+import os
 
 
-setup(
-        name='nose2pytest',
-        version='1.0.12',
-        packages=['nose2pytest'],
-        long_description=open('README.rst', encoding="UTF-8").read(),
-        long_description_content_type="text/x-rst",
-        entry_points={
-            'console_scripts': [
-                'nose2pytest = nose2pytest.script:main',
+VERSION = '1.6.1'
+AUTHOR_NAME = 'Andy Port'
+AUTHOR_EMAIL = 'AndyAPort@gmail.com'
+GITHUB = 'https://github.com/mathandy/svgpathtools'
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(relative_path):
+    """Reads file at relative path, returning contents as string."""
+    with codecs.open(os.path.join(_here, relative_path), "rb", "utf-8") as f:
+        return f.read()
+
+
+setup(name='svgpathtools',
+      packages=['svgpathtools'],
+      version=VERSION,
+      description=('A collection of tools for manipulating and analyzing SVG '
+                   'Path objects and Bezier curves.'),
+      long_description=read("README.md"),
+      long_description_content_type='text/markdown',
+      author=AUTHOR_NAME,
+      author_email=AUTHOR_EMAIL,
+      url=GITHUB,
+      download_url='{}/releases/download/{}/svgpathtools-{}-py2.py3-none-any.whl'
+                   ''.format(GITHUB, VERSION, VERSION),
+      license='MIT',
+      install_requires=['numpy', 'svgwrite', 'scipy'],
+      platforms="OS Independent",
+      keywords=['svg', 'svg path', 'svg.path', 'bezier', 'parse svg path', 'display svg'],
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Image Recognition",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "Topic :: Software Development :: Libraries :: Python Modules",
             ],
-            'pytest11': ['pytest_nose_assert_tools = nose2pytest.assert_tools'],
-        },
-        url='https://github.com/schollii/nose2pytest',
-        license='BSD-3',
-        author='Oliver Schoenborn',
-        author_email='oliver.schoenborn@gmail.com',
-        description='Convert nose.tools.assert_ calls found in your Nose test modules into raw asserts for pytest',
-        keywords='nose to pytest conversion',
-        install_requires=[
-            'fissix',
-        ],
-        python_requires='>=3.8,<3.12',
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-
-            # Indicate who your project is intended for
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Testing',
-
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: BSD License',
-
-            # Specify the Python versions you support here.
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-        ],
-)
+      )
