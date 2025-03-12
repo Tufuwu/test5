840d57b094bf70428c674e4998c91efa43e4e2da
wu <wu@.com>
2025-03-12 10:04:57 +0800

    letterror/ufoprocessor0

--- setup.py
+++ setup.py
@@ -1,43 +1,53 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-"""
-"""
+#!/usr/bin/env python
 
+import sys
 from setuptools import setup, find_packages
+from io import open
 
-# Setuptools config
-NAME = "numerizer"
-DESCRIPTION = "Python module for converting natural language numbers into ints and floats."
-with open('README.rst', encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
-MAINTAINER = 'Jaidev Deshpande'
-MAINTAINER_EMAIL = 'deshpande.jaidev@gmail.com'
-URL = "https://github.com/jaidevd/numerizer"
-DOWNLOAD_URL = 'https://pypi.org/project/numerizer/#files'
-LICENSE = 'MIT'
-PROJECT_URLS = {
-    'Bug Tracker': 'https://github.com/jaidevd/numerizer/issues',
-    'Documentation': 'https://github.com/jaidevd/numerizer/tree/master/README.rst',
-    'Source Code': 'https://github.com/jaidevd/numerizer'
-}
-VERSION = '0.2.4'
 
-# Requirements
-install_requires = []
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
 
-# Setup
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name=NAME,
-    maintainer=MAINTAINER,
-    maintainer_email=MAINTAINER_EMAIL,
-    description=DESCRIPTION,
-    license=LICENSE,
-    url=URL,
-    download_url=DOWNLOAD_URL,
-    version=VERSION,
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=install_requires
+    name="ufoProcessor",
+    use_scm_version={"write_to": "Lib/ufoProcessor/_version.py"},
+    description="Read, write and generate UFOs with designspace data.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Erik van Blokland",
+    author_email="erik@letterror.com",
+    url="https://github.com/LettError/ufoProcessor",
+    keywords='font development tools',
+    license="MIT",
+    packages=find_packages("Lib"),
+    package_dir={"": "Lib"},
+    python_requires='>=2.7',
+    setup_requires=wheel + ["setuptools_scm"],
+    install_requires=[
+        "defcon[lxml]>=0.6.0",
+        "fontMath>=0.4.9",
+        "fontParts>=0.8.2",
+        "fontTools[ufo,lxml]>=3.32.0",
+        "mutatorMath>=2.1.2",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
