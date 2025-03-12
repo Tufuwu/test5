ddeebf3df2351ab5f0c29b891d77623b09784208
wu <wu@.com>
2025-03-12 16:54:18 +0800

    vifactor/repostat0

--- src/cid/backends/mysql/base.py
+++
@@ -1,9 +0,0 @@
-from django.db.backends.mysql.base import DatabaseWrapper as BaseDatabaseWrapper
-
-from ...cursor import CidCursorWrapper
-
-
-class DatabaseWrapper(BaseDatabaseWrapper):
-    def create_cursor(self, name=None):
-        base_cursor = super().create_cursor(name)
-        return CidCursorWrapper(base_cursor)
