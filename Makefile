783d67b445543fbd067ee7a8d4cbe691ad8ce324
wu <wu@.com>
2025-03-12 12:37:04 +0800

    zooniverse/panoptes-cli0

--- Makefile
+++
@@ -1,7 +0,0 @@
-
-build:
-	python3 -m pip install --upgrade build
-	python3 -m build
-
-test:
-	pip install ./dist/timeplus-proton-driver-0.2.7.tar.gz
--- setup.py
+++ setup.py
@@ -1,134 +1,30 @@
-import os
-import re
-from codecs import open
-
 from setuptools import setup, find_packages
-from distutils.extension import Extension
-
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    USE_CYTHON = False
-else:
-    USE_CYTHON = True
-
-CYTHON_TRACE = bool(os.getenv('CYTHON_TRACE', False))
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read_version():
-    regexp = re.compile(r'^VERSION\W*=\W*\(([^\(\)]*)\)')
-    init_py = os.path.join(here, 'proton_driver', '__init__.py')
-    with open(init_py, encoding='utf-8') as f:
-        for line in f:
-            match = regexp.match(line)
-            if match is not None:
-                return match.group(1).replace(', ', '.')
-        else:
-            raise RuntimeError(
-                'Cannot find version in proton_driver/__init__.py'
-            )
-
-
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# Prepare extensions.
-ext = '.pyx' if USE_CYTHON else '.c'
-extensions = [
-    Extension(
-        'proton_driver.bufferedreader',
-        ['proton_driver/bufferedreader' + ext]
-    ),
-    Extension(
-        'proton_driver.bufferedwriter',
-        ['proton_driver/bufferedwriter' + ext]
-    ),
-    Extension(
-        'proton_driver.columns.largeint',
-        ['proton_driver/columns/largeint' + ext]
-    ),
-    Extension(
-        'proton_driver.varint',
-        ['proton_driver/varint' + ext]
-    )
-]
-
-if USE_CYTHON:
-    compiler_directives = {'language_level': '3'}
-    if CYTHON_TRACE:
-        compiler_directives['linetrace'] = True
-
-    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='proton-driver',
-    version=read_version(),
-
-    description='Python driver with native interface for Proton',
+    name='panoptescli',
+    version='1.1.5',
+    url='https://github.com/zooniverse/panoptes-cli',
+    author='Adam McMaster / Zooniverse',
+    author_email='contact@zooniverse.org',
+    description=(
+        'A command-line client for Panoptes, the API behind the Zooniverse'
+    ),
     long_description=long_description,
-
-    url='https://github.com/timeplus-io/proton-python-driver',
-
-    author='Gang Tao',
-    author_email='gang@timeplus.com',
-
-    license='MIT',
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-
-
-        'Environment :: Console',
-
-
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-
-
-        'License :: OSI Approved :: MIT License',
-
-
-        'Operating System :: OS Independent',
-
-
-        'Programming Language :: SQL',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: Implementation :: PyPy',
-
-        'Topic :: Database',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Information Analysis'
-    ],
-
-    keywords='Proton db database cloud analytics',
-
-    packages=find_packages('.', exclude=['tests*']),
-    python_requires='>=3.8, <4',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'pytz',
-        'tzlocal',
-        'tzlocal<2.1; python_version=="3.5"'
+        'Click>=6.7,<8.2',
+        'PyYAML>=5.1,<6.1',
+        'panoptes-client>=1.6,<2.0',
+        'humanize>=0.5.1,<4.8',
+        'pathvalidate>=0.29.0,<2.6',
     ],
-    ext_modules=extensions,
-    extras_require={
-        'lz4': [
-            'lz4<=3.0.1; implementation_name=="pypy"',
-            'lz4; implementation_name!="pypy"',
-            'clickhouse-cityhash>=1.0.2.1'
-        ],
-        'zstd': ['zstd', 'clickhouse-cityhash>=1.0.2.1'],
-        'numpy': ['numpy>=1.12.0', 'pandas>=0.24.0']
-    },
-    test_suite='pytest'
+    entry_points='''
+        [console_scripts]
+        panoptes=panoptes_cli.scripts.panoptes:cli
+    ''',
 )
