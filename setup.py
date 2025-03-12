6a6c6d065b2e3e231016533a4546bbf57152083d
wu <wu@.com>
2025-03-12 10:59:32 +0800

    mgaitan/sphinxcontrib-mermaid0

--- setup.py
+++ setup.py
@@ -1,8 +1,1 @@
-from pathlib import Path
-from setuptools import setup
-
-HERE = Path(__file__).parent
-version = (HERE / 'VERSION').read_text().strip()
-filename = (HERE / 'simplesat' / '_version.py').write_text(f'__version__ = "{version}"\n')
-
-setup()
+__import__("setuptools").setup()
