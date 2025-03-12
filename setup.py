b3bdacf4a60b9c33713a7367e35006036e8f203d
wu <wu@.com>
2025-03-12 12:21:24 +0800

    django-tastypie/django-tastypie0

--- setup.py
+++ setup.py
@@ -1,36 +1,66 @@
-"""
-Copyright (c) 2019-2021, Jairus Martin.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-Distributed under the terms of the MIT License.
 
-The full license is in the file LICENSE.text, distributed with this software.
+from tastypie import __version__
 
-Created on Feb 21, 2019
-
-@author: jrm
-"""
-from setuptools import setup, find_packages
 
 setup(
-    name="atom-db",
-    version="0.8.1",
-    author="CodeLV",
-    author_email="frmdstryr@gmail.com",
-    url="https://github.com/codelv/atom-db",
-    description="Database abstraction layer for atom objects",
-    license="MIT",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    requires=["atom"],
-    python_requires=">=3.7",
-    install_requires=["atom>=0.7.0"],
-    optional_requires=[
-        "sqlalchemy<2",
-        "aiomysql",
-        "aiopg",
-        "aiosqlite",  # sql database support
-        "motor",  # nosql database support
+    name='django-tastypie',
+    version=__version__,
+    description='A flexible & capable API layer for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='https://github.com/django-tastypie/django-tastypie',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'tastypie',
+        'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+        'tastypie.migrations',
+        'tastypie.contrib',
+        'tastypie.contrib.gis',
+        'tastypie.contrib.contenttypes',
     ],
-    packages=find_packages(),
-    package_data={'atomdb': ["py.typed"]}
+    package_data={
+        'tastypie': ['templates/tastypie/*'],
+    },
+    zip_safe=False,
+    requires=[
+        'python_mimeparse(>=0.1.4, !=1.5)',
+        'dateutil(>=2.1)',
+    ],
+    install_requires=[
+        'python-mimeparse >= 0.1.4, != 1.5',
+        'python-dateutil >= 2.1',
+    ],
+    tests_require=['PyYAML', 'lxml', 'defusedxml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Utilities'
+    ],
 )
