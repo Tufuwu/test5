6f1f1ff90bf40f323dc4602ca86a1542d1ee311b
wu <wu@.com>
2025-03-12 10:05:34 +0800

    beaglecli/beagle0

--- setup.py
+++ setup.py
@@ -1,53 +1,28 @@
-#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import sys
-from setuptools import setup, find_packages
-from io import open
+import setuptools
 
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
-wheel = ['wheel'] if needs_wheel else []
-
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name="ufoProcessor",
-    use_scm_version={"write_to": "Lib/ufoProcessor/_version.py"},
-    description="Read, write and generate UFOs with designspace data.",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author="Erik van Blokland",
-    author_email="erik@letterror.com",
-    url="https://github.com/LettError/ufoProcessor",
-    keywords='font development tools',
-    license="MIT",
-    packages=find_packages("Lib"),
-    package_dir={"": "Lib"},
-    python_requires='>=2.7',
-    setup_requires=wheel + ["setuptools_scm"],
-    install_requires=[
-        "defcon[lxml]>=0.6.0",
-        "fontMath>=0.4.9",
-        "fontParts>=0.8.2",
-        "fontTools[ufo,lxml]>=3.32.0",
-        "mutatorMath>=2.1.2",
-    ],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Graphics",
-        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
