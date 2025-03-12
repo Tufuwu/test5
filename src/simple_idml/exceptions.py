bb729c5fe37448f046e069faef9aab1c23af8fd6
wu <wu@.com>
2025-03-12 11:54:43 +0800

    n-wouda/alns0

--- src/simple_idml/exceptions.py
+++
@@ -1,8 +0,0 @@
-class InDesignSoapException(BaseException):
-    _error = None
-
-    def __init__(self, script_params, response):
-        self._error = f"Script params: {script_params}\nResponse: {response}"
-
-    def __str__(self):
-        return repr(self._error)
