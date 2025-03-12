6dba556abd9428a0fe6b45bad55ab4cb55145a1d
wu <wu@.com>
2025-03-12 12:42:52 +0800

    tutorcruncher/pydf0

--- cherry_picker/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backport CPython changes from main to maintenance branches."""
-
-from __future__ import annotations
-
-from ._version import __version__
-
-__all__ = ["__version__"]
--- cherry_picker/__main__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .cherry_picker import cherry_pick_cli
-
-if __name__ == "__main__":
-    cherry_pick_cli()
