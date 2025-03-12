d3de88fdab2b0b96a3a0e2a0d121ae8c80d124a1
wu <wu@.com>
2025-03-12 19:26:43 +0800

    provinzkraut/cologne_phonetics0

--- Contributors.md
+++
@@ -1,3 +0,0 @@
-# Contributors
-- [Alexei Chetroi] (https://github.com/Adminiuga)
-- [puddly] (https://github.com/puddly)
--- tests/tools/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def patch_logging(mocker):
-    # To prevent global state from being modified in tool tests
-    mocker.patch("logging.addLevelName")
-    mocker.patch("coloredlogs.install")
