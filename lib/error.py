cc1c3d09181274a0d2180b08dec46dfbc01da18c
wu <wu@.com>
2025-03-12 18:49:51 +0800

    idr/idr-metadata0

--- lib/error.py
+++
@@ -1,3 +0,0 @@
-class TestRunInitError(Exception):
-    def __init__(self, *args, **kwargs):
-        super(TestRunInitError, self).__init__(*args, **kwargs)
--- test/unittest/replica-7f4d4895ff58.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env tarantool
-
-box.cfg {
-    listen = os.getenv('LISTEN'),
-    replication = os.getenv('MASTER'),
-}
-
-require('console').listen(os.getenv('ADMIN'))
