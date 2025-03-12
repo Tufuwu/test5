58da8e9e6fe2717066b3ae1c8b08727154b9bf68
wu <wu@.com>
2025-03-12 13:15:56 +0800

    jschneier/django-storages0

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from impedance import __version__
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding='utf8') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="impedance",
-    version=__version__,
-    author="impedance.py developers",
-    author_email="matt.murbach@gmail.com",
-    description="A package for analyzing electrochemical impedance data",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://impedancepy.readthedocs.io/en/latest/",
-    packages=setuptools.find_packages(),
-    python_requires="~=3.8",
-    install_requires=['altair>=3.0', 'matplotlib>=3.5',
-                      'numpy>=1.22.4', 'scipy>=1.0',
-                      'pandas'],
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
-)
+setup()
