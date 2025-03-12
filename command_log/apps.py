3a56a688aa3bb615cf463c12d06bef60b8e16723
wu <wu@.com>
2025-03-12 17:15:58 +0800

    inveniosoftware/invenio-app-ils0

--- command_log/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManagementCommandLogConfig(AppConfig):
-    name = "command_log"
-    verbose_name = "Management command audit log"
-    default_auto_field = "django.db.models.AutoField"
--- tests/management/commands/test_transaction_command.py
+++
@@ -1,7 +0,0 @@
-from command_log.commands import TransactionLoggedCommand
-
-
-class Command(TransactionLoggedCommand):
-    def do_command(self, *args, **options):
-        commit = options["commit"]
-        self.stdout.write(f"Running test transaction command, --commit={commit}")
