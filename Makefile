d950a43464833233e96f251e0818709fa6240e4d
wu <wu@.com>
2025-03-12 19:04:47 +0800

    python-babel/babel0

--- Makefile
+++ Makefile
@@ -1,7 +1,23 @@
-all: doc
+test: import-cldr
+	python ${PYTHON_TEST_FLAGS} -m pytest ${PYTEST_FLAGS}
 
-doc:
-	$(MAKE) -C doc html
+clean: clean-cldr clean-pyc
 
+import-cldr:
+	python scripts/download_import_cldr.py
 
-.PHONY: all doc
+clean-cldr:
+	rm -f babel/locale-data/*.dat
+	rm -f babel/global.dat
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm {} \;
+	find . -name '__pycache__' -type d | xargs rm -rf
+
+develop:
+	pip install --editable .
+
+tox-test:
+	tox
+
+.PHONY: test develop tox-test clean-pyc clean-cldr import-cldr clean standalone-test
--- setup.py
+++ setup.py
@@ -1,34 +1,112 @@
-#!/usr/bin/env python3
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import subprocess
+import sys
 
-import setuptools
+from setuptools import Command, setup
+
+try:
+    from babel import __version__
+except SyntaxError as exc:
+    sys.stderr.write(f"Unable to import Babel ({exc}). Are you running a supported version of Python?\n")
+    sys.exit(1)
 
 
-def local_scheme(version):
-    """Generate a PEP440 compatible version if PEP440_VERSION is enabled"""
-    import os
+class import_cldr(Command):
+    description = 'imports and converts the CLDR data'
+    user_options = []
 
-    import setuptools_scm.version  # only present during setup time
+    def initialize_options(self):
+        pass
 
-    return (
-        ""
-        if "PEP440_VERSION" in os.environ
-        else setuptools_scm.version.get_local_node_and_date(version)
-    )
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        subprocess.check_call([sys.executable, 'scripts/download_import_cldr.py'])
 
 
-if __name__ == "__main__":
-    setuptools.setup(
-        use_scm_version={"local_scheme": local_scheme},
-        setup_requires=["setuptools_scm"],
-    )
+setup(
+    name='babel',
+    version=__version__,
+    description='Internationalization utilities',
+    long_description='A collection of tools for internationalizing Python applications.',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    maintainer='Aarni Koskela',
+    maintainer_email='akx@iki.fi',
+    license='BSD-3-Clause',
+    url='https://babel.pocoo.org/',
+    project_urls={
+        'Source': 'https://github.com/python-babel/babel',
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.8',
+    packages=['babel', 'babel.messages', 'babel.localtime'],
+    package_data={"babel": ["py.typed"]},
+    include_package_data=True,
+    install_requires=[
+        # This version identifier is currently necessary as
+        # pytz otherwise does not install on pip 1.4 or
+        # higher.
+        # Python 3.9 and later include zoneinfo which replaces pytz
+        'pytz>=2015.7; python_version<"3.9"',
+    ],
+    extras_require={
+        'dev': [
+            "tzdata;sys_platform == 'win32'",
+            'backports.zoneinfo; python_version<"3.9"',
+            'freezegun~=1.0',
+            'jinja2>=3.0',
+            'pytest-cov',
+            'pytest>=6.0',
+            'pytz',
+            'setuptools',
+        ],
+    },
+    cmdclass={'import_cldr': import_cldr},
+    zip_safe=False,
+    # Note when adding extractors: builtin extractors we also want to
+    # work if packages are not installed to simplify testing.  If you
+    # add an extractor here also manually add it to the "extract"
+    # function in babel.messages.extract.
+    entry_points="""
+    [console_scripts]
+    pybabel = babel.messages.frontend:main
+
+    [distutils.commands]
+    compile_catalog = babel.messages.setuptools_frontend:compile_catalog
+    extract_messages = babel.messages.setuptools_frontend:extract_messages
+    init_catalog = babel.messages.setuptools_frontend:init_catalog
+    update_catalog = babel.messages.setuptools_frontend:update_catalog
+
+    [distutils.setup_keywords]
+    message_extractors = babel.messages.setuptools_frontend:check_message_extractors
+
+    [babel.checkers]
+    num_plurals = babel.messages.checkers:num_plurals
+    python_format = babel.messages.checkers:python_format
+
+    [babel.extractors]
+    ignore = babel.messages.extract:extract_nothing
+    python = babel.messages.extract:extract_python
+    javascript = babel.messages.extract:extract_javascript
+    """,
+)
