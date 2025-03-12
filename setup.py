55eaa2e7501f0cd3d442be0ef08930ba1afdddb3
wu <wu@.com>
2025-03-12 11:29:30 +0800

    gintas/django-picklefield0

--- setup.py
+++ setup.py
@@ -1,108 +1,48 @@
-"""A setuptools based setup module.
+from setuptools import find_packages, setup
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+import picklefield
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-from os import listdir, path
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# Collect names of bin/*py scripts
-# e.g. 'pdb_intersect=bin.pdb_intersect:main',
-binfiles = listdir(path.join(here, 'pdbtools'))
-bin_py = [f[:-3] + '=pdbtools.' + f[:-3] + ':main' for f in binfiles
-          if f.endswith('.py')]
+with open('README.rst') as file_:
+    long_description = file_.read()
 
 setup(
-    name='pdb-tools',  # Required
-    version='2.5.1',  # Required
-    description='A swiss army knife for PDB files.',  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-    url='http://bonvinlab.org/pdb-tools',  # Optional
-    author='Joao Rodrigues',  # Optional
-    author_email='j.p.g.l.m.rodrigues@gmail.com',  # Optional
-    license='Apache Software License, version 2',
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+    name='django-picklefield',
+    version=picklefield.__version__,
+    description='Pickled object field for Django',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Simon Charette',
+    author_email='charette.s+django-picklefiel@gmail.com',
+    url='http://github.com/gintas/django-picklefield',
+    license='MIT',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
-        #'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: Apache Software License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
-    keywords='bioinformatics protein structural-biology pdb',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(),  # Required
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': bin_py,
+    keywords=['django pickle model field'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    python_requires='>=3.8',
+    install_requires=['Django>=3.2'],
+    extras_require={
+        'tests': ['tox'],
     },
-
-    # scripts=[path.join('bin', f) for f in listdir(path.join(here, 'bin'))],
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/haddocking/pdb-tools/issues',
-        'Source': 'https://github.com/haddocking/pdb-tools',
-    },
-
-    # Test Suite
-    test_suite='tests.test_all',
 )
