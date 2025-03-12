f4b2fb431a09410d382442bee72444888a7f1b9b
wu <wu@.com>
2025-03-12 20:12:01 +0800

    fkie/catkin_lint0

--- pjuu/posts/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf8 -*-
-
-"""Deals with anything to do with posts, replies, votes and subscriptions.
-
-:license: AGPL v3, see LICENSE for more details
-:copyright: 2014-2023 Joe Doherty
-
-"""
--- pjuu/users/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf8 -*-
-
-"""Users package, handles followers, feeds, profiles etc.
-
-:license: AGPL v3, see LICENSE for more details
-:copyright: 2014-2023 Joe Doherty
-
-"""
