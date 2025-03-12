de86e7ef0b21e7db3d34ace8e1028556ecea7b2c
wu <wu@.com>
2025-03-12 15:00:48 +0800

    modlinltd/django-advanced-filters0

--- setup.py
+++ setup.py
@@ -1,93 +1,95 @@
+import os
+import sys
 
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+from advanced_filters import __version__
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
+def get_full_description():
+    # get long description from README
+    readme = 'README.rst'
+    changelog = 'CHANGELOG.rst'
+    base = os.path.dirname(__file__)
+    with open(os.path.join(base, readme), encoding='utf-8') as readme:
+        README = readme.read()
+    with open(os.path.join(base, changelog), encoding='utf-8') as changelog:
+        CHANGELOG = changelog.read()
+    return f'{README}\n{CHANGELOG}'
+
+
+# allow setup.py to be run from any path
+CUR_DIR = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
+os.chdir(CUR_DIR)
+TEST_REQ_FILE = os.path.join(CUR_DIR, 'test-reqs.txt')
+if os.path.exists(TEST_REQ_FILE):
+    with open(TEST_REQ_FILE) as f:
+        TEST_REQS = list(f.readlines())
+else:
+    TEST_REQS = []
 
 
 setup(
-    name='asoc_members',
-    version='0.1.0',
-
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Memberships management for the Python Argentina's ONG",
-
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://ac.python.org.ar/',
-
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='Asociación Civil Python Argentina',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='presidencia@ac.python.org.ar',  # Optional
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
-    classifiers=[  # Optional
-        'Development Status :: 1 - Planning',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Framework :: Django :: 2.0',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    keywords='python argentina pyar django memberships ong',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
+    name='django-advanced-filters',
+    version=__version__,
+    url='https://github.com/modlinltd/django-advanced-filters',
+    license='MIT',
+    description='A Django application for advanced admin filters',
+    keywords='django-admin admin advanced filters custom query',
+    long_description=get_full_description(),
+    packages=find_packages(exclude=['tests*', 'tests.*', '*.tests']),
+    include_package_data=True,
     install_requires=[
-        'django',
-        'Pillow',
-        'django-extensions',
+        'simplejson>=3.6.5,<4',
     ],
-    scripts=['website/manage.py'],
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/PyAr/asoc_members/issues',
-        'About us': 'https://ac.python.org.ar/',
-        'Source': 'https://github.com/PyAr/asoc_members/',
-    },
+    extras_require=dict(test=TEST_REQS),
+    zip_safe=False,
+    author='Pavel Savchenko',
+    author_email='pavel@modlinltd.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
 )
