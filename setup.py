2877d610538cc1c046ae0cf0dab79ac9bf631aef
wu <wu@.com>
2025-03-12 11:21:21 +0800

    elmotec/circlify0

--- setup.py
+++ setup.py
@@ -1,21 +1,46 @@
-import setuptools
+#!/usr/bin/env python
+# encoding: utf-8
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+"""Packaging script."""
 
-setuptools.setup(
-    name="currency-symbols",
-    version="2.0.2",
-    author="Arshad Kazmi",
-    author_email="arshadkazmi42@gmail.com",
-    description="Get currency symbol by currency code",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/arshadkazmi42/currency-symbols",
-    packages=setuptools.find_packages(),
+import os
+
+from setuptools import setup
+
+__version__ = "0.15.0"
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, "README.rst")).read()
+
+setup(
+    name="circlify",
+    description="Circle packing algorithm for Python",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    version=__version__,
+    author="Elmotec",
+    author_email="elmotec@gmx.com",
+    license="MIT",
+    keywords="circle packing enclosure hierarchy graph display visualization",
+    url="http://github.com/elmotec/circlify",
+    py_modules=["circlify"],
+    test_suite="tests",
+    setup_requires=[],
+    tests_require=[],
+    python_requires=">=3.5",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Developers",
     ],
 )
