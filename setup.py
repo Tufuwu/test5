d73fe59b1602726e9230e4f6aea0ecd6c9db7b78
wu <wu@.com>
2025-03-12 19:07:49 +0800

    ebook-utils/css-parser0

--- setup.py
+++ setup.py
@@ -1,74 +1,72 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: LGPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-from pathlib import Path
+import ast
+import re
+import sys
+import os
 
 from setuptools import find_packages, setup
+from setuptools.command.test import test
 
-root_dir = Path(__file__).parent
-src_dir = root_dir / "dbbackup"
-with (src_dir / "VERSION").open() as f:
-    version = f.read().strip()
-
-
-def get_requirements():
-    with (root_dir / "requirements.txt").open() as f:
-        return f.read().splitlines()
+# extract the version without importing the module
+VERSION = open('src/css_parser/version.py', 'rb').read().decode('utf-8')
+VERSION = '.'.join(map(str, ast.literal_eval(re.search(r'^version\s+=\s+(.+)', VERSION, flags=re.M).group(1))))
+long_description = '\n' + open('README.md', 'rb').read().decode('utf-8') + '\n'  # + read('CHANGELOG.txt')
 
 
-def get_test_requirements():
-    with (root_dir / "requirements" / "tests.txt").open() as f:
-        return f.read().splitlines()
+class Test(test):
+
+    user_options = [
+        ('which-test=', 'w', "Specify which test to run as either"
+            " the test method name (without the leading test_)"
+            " or a module name with a trailing period"),
+    ]
+
+    def initialize_options(self):
+        self.which_test = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import importlib
+        orig = sys.path[:]
+        try:
+            sys.path.insert(0, os.getcwd())
+            m = importlib.import_module('run_tests')
+            which_test = (self.which_test,) if self.which_test else ()
+            m.run_tests(which_test)
+        finally:
+            sys.path = orig
 
 
 setup(
-    name="django-dbbackup",
-    version=version,
-    description="Management commands to help backup and restore a project database and media.",
-    author="Archmonger",
-    author_email="archiethemonger@gmail.com",
-    long_description=(root_dir / "README.rst").read_text(encoding="utf-8"),
-    long_description_content_type="text/x-rst",
-    python_requires=">=3.7",
-    install_requires=get_requirements(),
-    tests_require=get_test_requirements(),
-    include_package_data=True,
-    zip_safe=False,
-    license="BSD",
-    url="https://github.com/jazzband/django-dbbackup",
-    keywords=[
-        "django",
-        "database",
-        "media",
-        "backup",
-        "amazon",
-        "s3",
-        "dropbox",
-    ],
-    packages=find_packages(),
+    name='css-parser',
+    version=VERSION,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    description='A CSS Cascading Style Sheets library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    cmdclass={'test': Test},
+    author='Various People',
+    author_email='redacted@anonymous.net',
+    url='https://github.com/ebook-utils/css-parser',
+    license='LGPL 3.0 or later',
+    keywords='CSS, Cascading Style Sheets, CSSParser, DOM Level 2 Stylesheets, DOM Level 2 CSS',
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Environment :: Console",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.2",
-        "Framework :: Django :: 5.0",
-        "Framework :: Django :: 5.1",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Topic :: Database",
-        "Topic :: System :: Archiving",
-        "Topic :: System :: Archiving :: Backup",
-        "Topic :: System :: Archiving :: Compression",
-    ],
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
 )
